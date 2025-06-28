6f985a2265a3e3c833edf4baf67a12257777ce7c
hardy-is-cat <whwjdguszz@naver.com>
2025-06-28 14:42:24 +0900

    [Fix] 수정사항 반영 - 다크모드 오류 수정 - netlify -> vercel 변경 위한 _redirects 파일 설정 - vercel analitics 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,8 @@
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
-import { useState } from "react";
 
 import createCache from "@emotion/cache";
-import { CacheProvider, ThemeProvider } from "@emotion/react";
-import { darkTheme, lightTheme } from "styles/theme";
+import { CacheProvider } from "@emotion/react";
 import globalStyles from "styles/global-styles";
 
 import Layout from "@/components/Layout";
@@ -12,22 +10,16 @@ import Layout from "@/components/Layout";
 const cache = createCache({ key: "next" });
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [isDarkMode, setIsDarkMode] = useState(false);
   return (
     <>
-      <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
-        <CacheProvider value={cache}>
-          {globalStyles}
-          <RecoilRoot>
-            <Layout>
-              <button onClick={() => setIsDarkMode(!isDarkMode)}>
-                {isDarkMode ? "라이트모드" : "다크모드"}
-              </button>
-              <Component {...pageProps} />
-            </Layout>
-          </RecoilRoot>
-        </CacheProvider>
-      </ThemeProvider>
+      <CacheProvider value={cache}>
+        {globalStyles}
+        <RecoilRoot>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </RecoilRoot>
+      </CacheProvider>
     </>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,14 @@
+import { Analytics } from "@vercel/analytics/next";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="ko-kr">
+    <Html lang="ko-KR">
       <Head />
       <body>
         <Main />
         <NextScript />
+        <Analytics />
       </body>
     </Html>
   );
